0895407deff67697dfc193442d80ce823af0fb7b
mabar84 <mabar@tut.by>
2023-09-09 14:55:40 +0300

    push from vs code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Hello world and webstorm</div>;
+  return <div className="App">Hello world from vs code</div>;
 }
 
 export default App;
