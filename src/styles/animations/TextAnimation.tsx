4a10bdc7f52c275a2afac80dab27748cb00117ed
mabar84 <mabar@tut.by>
2023-09-10 01:26:05 +0300

    sometimes doesn't work autosave and autoformatting

--- src/styles/animations/TextAnimation.tsx
+++ src/styles/animations/TextAnimation.tsx
@@ -2,11 +2,11 @@ import { keyframes } from "styled-components";
 
 export const TextAnimation = keyframes`
     from {
-margin-top: 0;
+        margin-top: 0;
     }
     to {
-margin-top: 20px;
-transform: rotate(5deg)
+        margin-top: 20px;
+        transform: rotate(5deg)
     }
     
 `;
