bcc781cb17245335a0d7cbb1fc0a93a7808c9441
mabar84 <mabar@tut.by>
2023-09-09 15:12:23 +0300

    emmet tsx doesn't work in VSCode

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 
 function App() {
   return <div className="App">Hello world from vs code</div>;
 }
 
 export default App;
+
+const StyledButton = styled.button``;
