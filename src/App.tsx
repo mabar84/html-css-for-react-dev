5608cd9f7bd1f6f2282eae61d86f5364ac08f932
mabar84 <mabar@tut.by>
2023-09-09 14:54:17 +0300

    test git after vs code in webstorm

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return <div className="App">Hello world and webstorm</div>;
 }
 
 export default App;
